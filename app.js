d86b9776c026f07b5aae3317b5ca5320c244b047
unknown <islam15-3879@diu.edu.bd>
2022-03-01 17:25:30 +0600

    added

--- app.js
+++ app.js
@@ -14,10 +14,14 @@ const showPhone=(phones)=>{
     const div = document.createElement('div');
     div.innerHTML = ` <div class="card border p-5">
 <div class="pro-pic">
-    <img class="w-25" src="${phone.image}" alt="">
+    <img class="w-50 ms-5" src="${phone.image}" alt="">
     <h2>Name: ${phone.phone_name}</h2>
     <h5>${phone.slug}</h5>
-    <p>description</p>
+    
+    <div class="allButtion">
+        <button " class="btn btn-danger ms-4">Delete</button>
+         <button onclick="details('')" class="btn btn-success ms-4">details</button>
+    </div>
     
 </div>
 </div>`;
